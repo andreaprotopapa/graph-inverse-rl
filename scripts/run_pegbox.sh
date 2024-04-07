c14b0043b184e3d0216b3b8de998ecf923ef3f41
Andrea Protopapa <protopapa.andrea@libero.it>
2024-04-07 04:26:21 +0200

    edit scripts

--- scripts/run_pegbox.sh
+++ scripts/run_pegbox.sh
@@ -1,2 +1,2 @@
 #  scripts/pegbox_helper.sh --task_name --cameras --camera_dropout --seed --action_space --num_head_layers --attention --concat --context1 --context2 --exp_suffix --algorithm --observation_type pretrained_path
-sh scripts_export/pegbox_helper.sh push 2 0 $2 xy 3 1 0 1 1 pegbox_graphirl sacv2 statefull+image $1
+sh scripts/pegbox_helper.sh push 2 0 $2 xy 3 1 0 1 1 pegbox_graphirl sacv2 statefull+image $1
