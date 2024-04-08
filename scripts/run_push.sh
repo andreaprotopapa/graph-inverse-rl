716ad27cdba41db8252b986c47c8cd4923d523b4
andreaprotopapa <protopapa.andrea@libero.it>
2024-04-08 08:18:01 +0200

    edit run script

--- scripts/run_push.sh
+++ scripts/run_push.sh
@@ -1,1 +1,1 @@
-sh scripts/push_helper.sh push 2 0 0 xy 3 1 0 1 1 push_graphirl sacv2 statefull+image 0 $1
+sh scripts/push_helper.sh push 2 0 $2 xy 3 1 0 1 1 push_graphirl sacv2 statefull+image 0 $1
